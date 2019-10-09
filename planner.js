2d841905811cdc798a3d2d03d8f81805eaa364fd
Tracy Ogutu <tracyogutu@gmail.com>
2019-10-09 09:23:23 +0300

    add variables for input fields in js

---
+++ planner.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("form#planner-detail").submit(function(event){
+   event.preventDefault();
+
+   var firstname=$("#firstname").val();
+   var lastname=$("#lastname").val();
+   var email=$("#emailinput").val();
+   var password =$("#passwordinput").val();
+   var description=$("#descriptioninput").val();
+
+
+
+
+
+
+
+
+
+
+
+});
+
+});
